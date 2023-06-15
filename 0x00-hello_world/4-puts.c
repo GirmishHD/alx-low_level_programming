2a283739f56789a71edea4c080200dea30e589f1
GirmishHD <gimmyphp@gmail.com>
2023-06-15 11:49:58 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main -Entry point 
- * Return -Always 0 (success)
+ * main:- Entry point 
+ * Return : -Always 0 (success)
  * */
 int main() {
   PUTS("Programming is like building a multilingual puzzle\n");
